d0465fe4af7c9acda2eb1176c49e47813d0dc1e8
Machavarapu Sai Chandu <104778552+AnonSai09@users.noreply.github.com>
2024-01-03 10:46:22 +0530

    Update application.py

--- application.py
+++ application.py
@@ -35,7 +35,7 @@ def predict():
     prediction = model.predict(input_data)[0]
     #now we will pass above predicted data to template
     return render_template('index.html',prediction = prediction)
-if__name__=="__main__":
+if __name__=="__main__":
     application.run()
 
 
